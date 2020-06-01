ab92479e46fa96517e98c772942be2a9ebf0e3eb
Ana Marr <acarolinasim@gmail.com>
2020-06-01 11:00:35 +0930

    updating models

--- models/lodafone.js
+++ models/lodafone.js
@@ -1,103 +1,103 @@
-// Dependencies
-// =============================================================
-
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
-
-var Brand = sequelize.define("brand", {
-    id: Sequelize.INTEGER,
-    brand_name: Sequelize.STRING
-});
-
-var ModelImages = sequelize.define("model_images", {
-    id: Sequelize.INTEGER,
-    local_model_id: Sequelize.INTEGER,
-    color_id: Sequelize.INTEGER,
-    image_url: Sequelize.STRING
+module.exports = function(sequelize, DataTypes) {
+  let Brand = sequelize.define("brand", {
+    id: DataTypes.INTEGER,
+    brand_name: DataTypes.STRING
   });
+  return Brand;
+};
 
-var FeatureCategory = sequelize.define("feature_category", {
-    id: Sequelize.INTEGER,
-    category_name: Sequelize.STRING
+module.exports = function(sequelize, DataTypes) {
+  let ModelImages = sequelize.define("model_images", {
+    id: DataTypes.INTEGER,
+    local_model_id: DataTypes.INTEGER,
+    color_id: DataTypes.INTEGER,
+    image_url: DataTypes.STRING
   });
+  return ModelImages;
+};
 
-var Feature = sequelize.define("feature", {
-    id: Sequelize.INTEGER,
-    feature_name: Sequelize.STRING,
-    category_id: Sequelize.INTEGER
+module.exports = function(sequelize, DataTypes) {
+  let FeatureCategory = sequelize.define("feature_category", {
+    id: DataTypes.INTEGER,
+    category_name: DataTypes.STRING
   });
+  return FeatureCategory;
+};
 
-var Color = sequelize.define("color", {
-    id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    hex_code: Sequelize.STRING
+module.exports = function(sequelize, DataTypes) {
+  let Feature = sequelize.define("feature", {
+    id: DataTypes.INTEGER,
+    feature_name: DataTypes.STRING,
+    category_id: DataTypes.INTEGER
   });
+  return Feature;
+};
 
-var Product = sequelize.define("product", {
-    sku: Sequelize.INTEGER,
-    local_model_id: Sequelize.INTEGER,
-    color_id: Sequelize.INTEGER,
-    price: Sequelize.DECIMAL,
-    units_in_stock: Sequelize.INTEGER
+module.exports = function(sequelize, DataTypes) {
+  let Color = sequelize.define("color", {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    hex_code: DataTypes.STRING
   });
+  return Color;
+};
 
-var Model = sequelize.define("model", {
-    local_model_id: Sequelize.INTEGER,
-    brand_id: Sequelize.STRING,
-    model_number: Sequelize.STRING,
-    model_name: Sequelize.STRING,
-    description: Sequelize.STRING
+module.exports = function(sequelize, DataTypes) {
+  let Product = sequelize.define("product", {
+    sku: DataTypes.INTEGER,
+    local_model_id: DataTypes.INTEGER,
+    color_id: DataTypes.INTEGER,
+    price: DataTypes.DECIMAL,
+    units_in_stock: DataTypes.INTEGER
   });
+  return Product;
+};
 
-var ModelFeature = sequelize.define("model_feature", {
-    model_id: Sequelize.INTEGER,
-    feature_id: Sequelize.INTEGER,
-    feature_description: Sequelize.STRING
+module.exports = function(sequelize, DataTypes) {
+  let  Model = sequelize.define("model", {
+    local_model_id: DataTypese.INTEGER,
+    brand_id: DataTypes.STRING,
+    model_number: DataTypes.STRING,
+    model_name: DataTypes.STRING,
+    description: DataTypes.STRING
   });
+  return Model;
+};
 
-var SoldProducts = sequelize.define("sold_products", {
-    id: Sequelize.INTEGER,
-    invoice_number: Sequelize.INTEGER,
-    sku: Sequelize.INTEGER,
-    quantity: Sequelize.INTEGER
+module.exports = function(sequelize, DataTypes) {
+  let  ModelFeature = sequelize.define("model_feature", {
+    model_id: DataTypes.INTEGER,
+    feature_id: DataTypes.INTEGER,
+    feature_description: DataTypes.STRING
   });
+  return ModelFeature;
+};
 
-var Invoice = sequelize.define("invoice", {
-   invoice_number: Sequelize.INTEGER,
-   customer_number: Sequelize.INTEGER
+module.exports = function(sequelize, DataTypes) {
+  let  SoldProducts = sequelize.define("sold_products", {
+    id: DataTypes.INTEGER,
+    invoice_number: DataTypes.INTEGER,
+    sku: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER
   });
+  return  SoldProducts;
+};
 
-var Customer = sequelize.define("customer", {
-    id: Sequelize.INTEGER,
-    first_name: Sequelize.STRING,
-    last_name: Sequelize.STRING,
-    email: Sequelize.STRING
-   });
+module.exports = function(sequelize, DataTypes) {
+  let  Invoice = sequelize.define("invoice", {
+    invoice_number: DataTypes.INTEGER,
+   customer_number: DataTypes.INTEGER
+  });
+  return  Invoice;
+};
 
-
-// Syncs with DB
-Brand.sync();
-ModelImages.sync();
-FeatureCategory.sync();
-Color.sync();
-Feature.sync();
-Product.sync();
-Model.sync();
-ModelFeature.sync();
-SoldProducts.sync();
-Invoice.sync();
-Customer.sync();
-
-// Makes the Models available for other files (will also create a table)
-module.exports = Brand;
-module.exports = ModelImages;
-module.exports = FeatureCategory;
-module.exports = Color;
-module.exports = Model;
-module.exports = ModelFeature
-module.exports = SoldProducts;
-module.exports = Invoice;
-module.exports = Customer;
+module.exports = function(sequelize, DataTypes) {
+  let Customer = sequelize.define("customer", {
+    id: DataTypes.INTEGER,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING
+  });
+  return  Customer;
+};
 
