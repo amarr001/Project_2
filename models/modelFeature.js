18f6113fa85bb06ab9c22b463c21f02e95b22496
Ana Marr <acarolinasim@gmail.com>
2020-06-03 19:46:40 +0930

    individual models

---
+++ models/modelFeature.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes) {
+    let ModelFeature = sequelize.define("model_feature", {
+    model_id: {type: DataTypes.INTEGER, primaryKey: true},
+    feature_id: DataTypes.INTEGER,
+    feature_description: DataTypes.STRING
+    },
+    { 
+      timestamps:false,
+      freezeTableName: true
+    }
+    );
+    return ModelFeature;
+  };
