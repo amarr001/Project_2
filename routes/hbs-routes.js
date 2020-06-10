8485e185356decce8b6a579e20b2b01d657987ff
Ana Marr <acarolinasim@gmail.com>
2020-06-10 16:59:45 +0930

    cart page handlebars and api route

--- routes/hbs-routes.js
+++ routes/hbs-routes.js
@@ -52,5 +52,29 @@ module.exports = function(app) {
         })
          
         });
+
+        app.get("/api/product/buy/:id", function(req, res) {
+
+          db.product.findAll({
+            where: {
+             price: req.params.id
+            },
+            include: [db.model]
+  
+  
+          }).then(function(data){
+  
+            var branddata = data.values;
+           
+           branddata.data = data.map(function(data){return data.toJSON() });
+            
+            var data = branddata.data
+            
+            res.render("cart",{data})
+            console.log(data);
+  
+          })
+           
+          });
   }
 
